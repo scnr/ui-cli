2454d205f0caa3ef7dfa9b7d4b63409dea727605
Tasos Laskos <tasos.laskos@gmail.com>
2017-05-23 01:50:04 +0300

    Added scnr_queue

---
+++ lib/scnr/ui/cli/rpc/server/queue.rb
@@ -0,0 +1,36 @@
+=begin
+    Copyright 2020 Tasos Laskos <tasos.laskos@gmail.com>
+
+    This file is part of the SCNR::UI::CLI project and is subject to
+    redistribution and commercial restrictions. Please see the SCNR::UI::CLI
+    web site for more information on licensing and terms of use.
+=end
+
+require_relative 'queue/option_parser'
+
+module SCNR
+
+require 'scnr/engine/rpc/server/queue'
+require 'scnr/ui/cli/utilities'
+
+module UI::CLI
+module RPC
+module Server
+
+# @author Tasos "Zapotek" Laskos<tasos.laskos@gmail.com>
+class Queue
+
+    def initialize
+        OptionParser.new.parse
+
+        Arachni::Reactor.global.run do
+            Engine::RPC::Server::Queue.new
+        end
+    end
+
+end
+
+end
+end
+end
+end
---
+++ lib/scnr/ui/cli/rpc/server/queue/option_parser.rb
@@ -0,0 +1,156 @@
+=begin
+    Copyright 2020 Tasos Laskos <tasos.laskos@gmail.com>
+
+    This file is part of the SCNR::UI::CLI project and is subject to
+    redistribution and commercial restrictions. Please see the SCNR::UI::CLI
+    web site for more information on licensing and terms of use.
+=end
+
+require_relative '../../../output'
+require_relative '../../../option_parser'
+
+module SCNR
+module UI::CLI
+module RPC
+module Server
+class Queue
+
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+class OptionParser < UI::CLI::OptionParser
+
+    attr_reader :cli
+
+    def initialize
+        super
+
+        separator 'Server'
+
+        on( '--address ADDRESS', 'Hostname or IP address to bind to.',
+               "(Default: #{options.rpc.server_address})"
+        ) do |address|
+            options.rpc.server_address = address
+        end
+
+        on( '--external-address ADDRESS', 'Hostname or IP address to advertise.',
+               "(Default: #{options.rpc.server_address})"
+        ) do |address|
+            options.dispatcher.external_address = address
+        end
+
+        on( '--port NUMBER', 'Port to listen to.', Integer,
+               "(Default: #{options.rpc.server_port})"
+        ) do |port|
+            options.rpc.server_port = port
+        end
+
+        on( '--port-range BEGINNING-END',
+               'Specify port range for the spawned RPC instances.',
+               "(Default: #{options.dispatcher.instance_port_range.join( '-' )})"
+        ) do |range|
+            options.dispatcher.instance_port_range = range.split( '-' ).map(&:to_i)
+        end
+
+        separator ''
+        separator 'Grid'
+
+        on( '--dispatcher-url HOST:PORT', 'Dispatcher to use.' ) do |url|
+            options.dispatcher.url = url
+        end
+
+        separator ''
+        separator 'Output'
+
+        on( '--output-reroute-to-logfile',
+            "Reroute scan output to log-files under: #{options.paths.logs}"
+        ) do
+            options.output.reroute_to_logfile = true
+        end
+
+        on( '--output-verbose', 'Show verbose output.',
+            "(Only applicable when '--reroute-to-logfile' is enabled.)"
+        ) do
+            verbose_on
+        end
+
+        on( '--output-debug [LEVEL 1-5]', Integer, 'Show debugging information.',
+            "(Only applicable when '--reroute-to-logfile' is enabled.)"
+        ) do |level|
+            debug_on( level || 1 )
+        end
+
+        on( '--output-only-positives', 'Only output positive results.',
+            "(Only applicable when '--reroute-to-logfile' is enabled.)"
+        ) do
+            only_positives
+        end
+
+        separator ''
+        separator 'SSL'
+
+        on( '--ssl-ca FILE',
+               'Location of the CA certificate (.pem).'
+        ) do |file|
+            options.rpc.ssl_ca = file
+        end
+
+        on( '--server-ssl-private-key FILE',
+               'Location of the server SSL private key (.pem).'
+        ) do |file|
+            options.rpc.server_ssl_private_key = file
+        end
+
+        on( '--server-ssl-certificate FILE',
+               'Location of the server SSL certificate (.pem).'
+        ) do |file|
+            options.rpc.server_ssl_certificate = file
+        end
+
+        on( '--client-ssl-private-key FILE',
+               'Location of the client SSL private key (.pem).'
+        ) do |file|
+            options.rpc.client_ssl_private_key = file
+        end
+
+        on( '--client-ssl-certificate FILE',
+               'Location of the client SSL certificate (.pem).'
+        ) do |file|
+            options.rpc.client_ssl_certificate = file
+        end
+
+        separator ''
+        separator 'Snapshot'
+
+        on( '--snapshot-save-path DIRECTORY', String,
+            'Directory under which to store snapshots of suspended scans.' ) do |path|
+            options.paths.snapshots = path
+        end
+
+        separator ''
+        separator 'System'
+
+        on( '--system-max-slots SLOTS', Integer,
+            'Maximum amount of Instances to be alive at any given time.',
+            '(Default: auto)'
+        ) do |max_slots|
+            options.system.max_slots = max_slots
+        end
+    end
+
+    def validate
+        validate_snapshot_save_path
+    end
+
+    def validate_snapshot_save_path
+        snapshot_path = options.paths.snapshots
+        return if !snapshot_path || File.directory?( snapshot_path )
+
+        $stderr.puts "Snapshot directory does not exist: #{snapshot_path}"
+        exit 1
+    end
+
+end
+end
+end
+end
+end
+end
