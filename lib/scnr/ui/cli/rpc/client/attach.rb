c16c08a6ab9a7dd2236e769e57397e653e632d7e
Tasos Laskos <tasos.laskos@gmail.com>
2017-05-28 15:48:49 +0300

    Added scnr_rpc_attach to re-connect to live Instances

---
+++ lib/scnr/ui/cli/rpc/client/attach.rb
@@ -0,0 +1,54 @@
+=begin
+    Copyright 2020 Tasos Laskos <tasos.laskos@gmail.com>
+
+    This file is part of the SCNR::UI::CLI project and is subject to
+    redistribution and commercial restrictions. Please see the SCNR::UI::CLI
+    web site for more information on licensing and terms of use.
+=end
+
+require_relative 'attach/option_parser'
+require_relative 'instance'
+
+module SCNR
+
+require 'scnr/engine/rpc/client/instance'
+require 'scnr/ui/cli/utilities'
+
+module UI::CLI
+module RPC::Client
+
+# Provides a command-line RPC client and uses a {RPC::Server::Dispatcher} to
+# provide an {RPC::Server::Instance} in order to perform a scan.
+#
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+class Attach
+    include Output
+
+    def initialize
+        parser = Attach::OptionParser.new
+        parser.ssl
+        parser.parse
+
+        instance = nil
+        begin
+            instance = SCNR::Engine::RPC::Client::Instance.new(
+                parser.url,
+                parser.token
+            )
+            instance.alive?
+        rescue Arachni::RPC::Exceptions::ConnectionError => e
+            print_error 'Could not connect to Instance.'
+            print_debug "Error: #{e.to_s}."
+            print_debug_backtrace e
+            exit 1
+        end
+
+        # Let the Instance UI manage the Instance from now on.
+        Instance.new( instance ).run
+    end
+
+end
+
+end
+end
+end
---
+++ lib/scnr/ui/cli/rpc/client/attach/option_parser.rb
@@ -0,0 +1,80 @@
+=begin
+    Copyright 2020 Tasos Laskos <tasos.laskos@gmail.com>
+
+    This file is part of the SCNR::UI::CLI project and is subject to
+    redistribution and commercial restrictions. Please see the SCNR::UI::CLI
+    web site for more information on licensing and terms of use.
+=end
+
+require 'scnr/engine/rpc/client'
+
+require_relative '../../../engine/option_parser'
+
+module SCNR
+module UI::CLI
+
+module RPC
+module Client
+class Attach
+
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+class OptionParser < UI::CLI::OptionParser
+
+    attr_reader :url
+    attr_reader :token
+
+    def ssl
+        separator ''
+        separator 'SSL'
+
+        on( '--ssl-ca FILE',
+            'Location of the CA certificate (.pem).'
+        ) do |file|
+            options.rpc.ssl_ca = file
+        end
+
+        on( '--ssl-private-key FILE',
+            'Location of the client SSL private key (.pem).'
+        ) do |file|
+            options.rpc.client_ssl_private_key = file
+        end
+
+        on( '--ssl-certificate FILE',
+            'Location of the client SSL certificate (.pem).'
+        ) do |file|
+            options.rpc.client_ssl_certificate = file
+        end
+    end
+
+    def validate
+        if options.dispatcher.url
+            begin
+                SCNR::Engine::RPC::Client::Dispatcher.new(
+                    options.dispatcher.url
+                ).alive?
+            rescue => e
+                print_error "Could not reach Dispatcher at: #{options.dispatcher.url}"
+                print_error "#{e.class}: #{e.to_s}"
+                exit 1
+            end
+        end
+
+        super
+    end
+
+    def after_parse
+        instance = ARGV.shift
+        @url, @token = instance.split( '/' )
+    end
+
+    def banner
+        "Usage: #{$0} [options] HOST:PORT/TOKEN"
+    end
+
+end
+
+end
+end
+end
+end
+end
