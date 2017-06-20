f0bbf8b50e2da0fa0338a6f95e813749469d7337
Tasos Laskos <tasos.laskos@gmail.com>
2017-06-20 14:25:33 +0300

    Added scnr_queue_list

---
+++ lib/scnr/ui/cli/rpc/client/queue/list/option_parser.rb
@@ -0,0 +1,111 @@
+=begin
+    Copyright 2020 Tasos Laskos <tasos.laskos@gmail.com>
+
+    This file is part of the SCNR::UI::CLI project and is subject to
+    redistribution and commercial restrictions. Please see the SCNR::UI::CLI
+    web site for more information on licensing and terms of use.
+=end
+
+require 'scnr/engine/rpc/client'
+
+require_relative '../../remote/option_parser'
+
+module SCNR
+module UI::CLI
+
+module RPC
+module Client
+module Queue
+class List
+
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+class OptionParser < UI::CLI::OptionParser
+
+    attr_accessor :queued
+    attr_accessor :running
+    attr_accessor :completed
+    attr_accessor :failed
+
+    def initialize
+        super
+
+        separator ''
+        separator 'Status'
+
+        on( '--queued', 'List queued scans.' ) do
+            @queued = true
+        end
+
+        on( '--running', 'List running scans.' ) do
+            @running = true
+        end
+
+        on( '--completed', 'List completed scans.' ) do
+            @completed = true
+        end
+
+        on( '--failed', 'List failed scans.' ) do
+            @failed = true
+        end
+    end
+
+    def list_all?
+        !@queued && !@running && !completed && !failed
+    end
+
+    def ssl
+        separator ''
+        separator 'SSL'
+
+        on( '--ssl-ca FILE',
+            'Location of the CA certificate (.pem).'
+        ) do |file|
+            options.rpc.ssl_ca = file
+        end
+
+        on( '--ssl-private-key FILE',
+            'Location of the client SSL private key (.pem).'
+        ) do |file|
+            options.rpc.client_ssl_private_key = file
+        end
+
+        on( '--ssl-certificate FILE',
+            'Location of the client SSL certificate (.pem).'
+        ) do |file|
+            options.rpc.client_ssl_certificate = file
+        end
+    end
+
+    def after_parse
+        options.queue.url = ARGV.shift
+    end
+
+    def validate
+        if !options.queue.url
+            print_error "Missing 'QUEUE_URL'."
+            exit 1
+        end
+
+        begin
+            SCNR::Engine::RPC::Client::Queue.new( options.queue.url ).alive?
+        rescue => e
+            print_error "Could not reach Queue at: #{options.queue.url}"
+            print_error "#{e.class}: #{e.to_s}"
+            exit 1
+        end
+
+        super
+    end
+
+    def banner
+        "Usage: #{$0} [options] QUEUE_URL"
+    end
+
+end
+
+end
+end
+end
+end
+end
+end
