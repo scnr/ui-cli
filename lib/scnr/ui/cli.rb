d6ea441d8ad5a831999177a58f3af4f4e66e51fb
Tasos Laskos <tasos.laskos@gmail.com>
2022-01-10 22:53:38 +0200

    Use relative paths

--- lib/scnr/ui/cli.rb
+++ lib/scnr/ui/cli.rb
@@ -8,11 +8,11 @@
 
 # Setup an output interface for the Engine before including it, otherwise it's
 # going to use its default which is basically a black hole.
-require 'scnr/ui/cli/output'
+require_relative 'cli/output'
 require 'scnr/engine'
 
-require 'scnr/ui/cli/version'
-require 'scnr/ui/cli/banner'
+require_relative 'cli/version'
+require_relative 'cli/banner'
 
 module SCNR
 module UI
