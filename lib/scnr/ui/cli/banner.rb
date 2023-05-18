7f151a5400cec2fb3ada93934a0e9635d2ce6bd4
Tasos Laskos <tasos.laskos@gmail.com>
2023-05-18 18:29:54 +0300

    Updated banner output

--- lib/scnr/ui/cli/banner.rb
+++ lib/scnr/ui/cli/banner.rb
@@ -16,7 +16,7 @@ module CLI
     WEBSITE = 'https://ecsypno.com'
 
     BANNER =<<EOBANNER
-SCNR::UI::CLI v#{CLI::VERSION} - SCNR::Engine v#{Engine::VERSION}
+            Codename SCNR v#{SCNR::VERSION}
     by Ecsypno Single Member P.C. (Copyright 2023)
              #{WEBSITE}
 EOBANNER
